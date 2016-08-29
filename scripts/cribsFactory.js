768a07e7fe93ed0fb9fae101dc107da466f4e938
alex shyaka <shyakaster@gmail.com>
2016-08-29 08:10:27 +0300

    first commit

---
+++ scripts/cribsFactory.js
@@ -0,0 +1,15 @@
+angular
+     .module('ngCribs')
+     .factory('cribsFactory', function($http) {
+
+        function getCribs()
+        {
+            return $http.get('data/data.json');
+        }
+
+        return {
+
+            getCribs: getCribs
+        }
+
+     });
